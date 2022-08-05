9d83ae110f879ffb88c4566921282514da4cb2ff
Dan Li <dianwen@gmail.com>
2022-08-05 13:12:31 -0700

    feat: zap logging

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"os"
+
+	"go.uber.org/zap"
+)
+
 func main() {
+	env := os.Getenv("ENV")
+	logger := setupLogger(env)
+
+	//nolint:errcheck // Ignore error from defer.
+	defer logger.Sync() // Flushes buffer, if any.
+
+	logger.Info("Starting node gateway.", zap.String("env", env))
+}
+
+func setupLogger(env string) *zap.Logger {
+	var (
+		logger *zap.Logger
+		err    error
+	)
+
+	if env == "production" {
+		logger, err = zap.NewProduction()
+	} else {
+		logger, err = zap.NewDevelopment()
+	}
+
+	if err != nil {
+		logger.Fatal("failed to create logger.", zap.Error(err))
+	}
+
+	return logger
 }
