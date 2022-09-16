ae8cbcbe6a661af18f859dfe777f3d2b15fb3f33
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2022-09-16 10:58:18 +0300

    chore: get ChainMetadataStore tests to work

--- internal/metadata/store.go
+++ internal/metadata/store.go
@@ -5,27 +5,67 @@ type BlockHeightUpdate struct {
 	BlockHeight uint64
 }
 
+type ReadRequest struct {
+	isGlobal bool
+	groupID  string
+}
+
 type ChainMetadataStore struct {
-	blockHeightChannel <-chan BlockHeightUpdate
-	maxHeightByGroupID map[string]uint64
-	globalMaxHeight    uint64
+	blockHeightChannel  <-chan BlockHeightUpdate
+	maxHeightByGroupID  map[string]uint64
+	globalMaxHeight     uint64
+	readRequestChannel  chan ReadRequest
+	readResponseChannel chan uint64
+	opChannel           chan func()
 }
 
 func NewChainMetadataStore(blockHeightChannel <-chan BlockHeightUpdate) *ChainMetadataStore {
 	return &ChainMetadataStore{
 		blockHeightChannel: blockHeightChannel,
 		maxHeightByGroupID: make(map[string]uint64),
+		opChannel:          make(chan func()),
 	}
 }
 
 func (c *ChainMetadataStore) Start() {
 	go func() {
-		for {
-			var update = <-c.blockHeightChannel
-			c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
-			c.updateHeightForGroup(update.GroupID, update.BlockHeight)
+		for update := range c.blockHeightChannel {
+			c.ProcessUpdate(update)
 		}
 	}()
+	go func(c *ChainMetadataStore) {
+		for op := range c.opChannel {
+			op()
+		}
+		//for {
+		//	select {
+		//	//case update := <-c.blockHeightChannel:
+		//	//	c.ProcessUpdate(update)
+		//	case op := <-c.opChannel:
+		//		op()
+		//	}
+		//}
+		//for r := range c.opChan {
+		//	r.fn()
+		//}
+		//for {
+		//	select {
+		//	case update := <-c.blockHeightChannel:
+		//		c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
+		//		c.updateHeightForGroup(update.GroupID, update.BlockHeight)
+		//	case readRequest := <-c.readRequestChannel:
+		//		if readRequest.isGlobal {
+		//			c.readResponseChannel <- c.globalMaxHeight
+		//		} else {
+		//			c.readResponseChannel <- c.maxHeightByGroupID[readRequest.groupID]
+		//		}
+		//	}
+		//	//var update = <-c.blockHeightChannel
+		//	//c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
+		//	////atomic.
+		//	//c.updateHeightForGroup(update.GroupID, update.BlockHeight)
+		//}
+	}(c)
 }
 
 func (c *ChainMetadataStore) updateHeightForGroup(groupID string, currentBlockHeight uint64) {
@@ -45,9 +85,26 @@ func max(a, b uint64) uint64 {
 }
 
 func (c *ChainMetadataStore) GetGlobalMaxHeight() uint64 {
-	return c.globalMaxHeight
+	var returnChannel = make(chan uint64)
+	c.opChannel <- func() {
+		returnChannel <- c.globalMaxHeight
+		close(returnChannel)
+	}
+	var result = <-returnChannel
+	return result
 }
 
 func (c *ChainMetadataStore) GetMaxHeightForGroup(groupID string) uint64 {
-	return c.maxHeightByGroupID[groupID]
+	var returnChannel = make(chan uint64)
+	c.opChannel <- func() {
+		returnChannel <- c.maxHeightByGroupID[groupID]
+	}
+	return <-returnChannel
+}
+
+func (c *ChainMetadataStore) ProcessUpdate(update BlockHeightUpdate) {
+	c.opChannel <- func() {
+		c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
+		c.updateHeightForGroup(update.GroupID, update.BlockHeight)
+	}
 }
---
+++ internal/metadata/store_test.go
@@ -0,0 +1,58 @@
+package metadata
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChainMetadataStore_GetGlobalMaxHeight(t *testing.T) {
+	var blockHeightChannel = make(chan BlockHeightUpdate)
+	var store = NewChainMetadataStore(blockHeightChannel)
+
+	store.Start()
+
+	assert.Equal(t, uint64(0), store.GetGlobalMaxHeight())
+
+	emitBlockHeight(store, "group1", uint64(10))
+	assert.Equal(t, uint64(10), store.GetGlobalMaxHeight())
+
+	emitBlockHeight(store, "group2", 12)
+	assert.Equal(t, uint64(12), store.GetGlobalMaxHeight())
+
+	emitBlockHeight(store, "group1", uint64(11))
+	// Non-increasing block heights are ignored.
+	assert.Equal(t, uint64(12), store.GetGlobalMaxHeight())
+}
+
+func TestChainMetadataStore_GetMaxHeightForGroup(t *testing.T) {
+	var blockHeightChannel = make(chan BlockHeightUpdate)
+	var store = NewChainMetadataStore(blockHeightChannel)
+
+	store.Start()
+
+	assert.Equal(t, uint64(0), store.GetMaxHeightForGroup("group1"))
+
+	emitBlockHeight(store, "group1", uint64(10))
+	assert.Equal(t, uint64(10), store.GetMaxHeightForGroup("group1"))
+	assert.Equal(t, uint64(0), store.GetMaxHeightForGroup("group2"))
+
+	emitBlockHeight(store, "group2", 12)
+	// Updates for other groups are ignored.
+	assert.Equal(t, uint64(10), store.GetMaxHeightForGroup("group1"))
+	assert.Equal(t, uint64(12), store.GetMaxHeightForGroup("group2"))
+
+	emitBlockHeight(store, "group1", uint64(11))
+	assert.Equal(t, uint64(11), store.GetMaxHeightForGroup("group1"))
+
+	emitBlockHeight(store, "group1", uint64(9))
+	// Non-increasing block heights are ignored.
+	assert.Equal(t, uint64(11), store.GetMaxHeightForGroup("group1"))
+}
+
+func emitBlockHeight(store *ChainMetadataStore, groupID string, blockHeight uint64) {
+	store.ProcessUpdate(BlockHeightUpdate{
+		GroupID:     groupID,
+		BlockHeight: blockHeight,
+	})
+}
