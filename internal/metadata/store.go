0787113de9ca478ac1f6db4865547677db344564
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2022-09-16 11:37:24 +0300

    chore: remove unused ChainMetadataStore fields

--- internal/metadata/store.go
+++ internal/metadata/store.go
@@ -5,66 +5,24 @@ type BlockHeightUpdate struct {
 	BlockHeight uint64
 }
 
-type ReadRequest struct {
-	isGlobal bool
-	groupID  string
-}
-
 type ChainMetadataStore struct {
-	blockHeightChannel  <-chan BlockHeightUpdate
-	maxHeightByGroupID  map[string]uint64
-	globalMaxHeight     uint64
-	readRequestChannel  chan ReadRequest
-	readResponseChannel chan uint64
-	opChannel           chan func()
+	maxHeightByGroupID map[string]uint64
+	globalMaxHeight    uint64
+	opChannel          chan func()
 }
 
-func NewChainMetadataStore(blockHeightChannel <-chan BlockHeightUpdate) *ChainMetadataStore {
+func NewChainMetadataStore() *ChainMetadataStore {
 	return &ChainMetadataStore{
-		blockHeightChannel: blockHeightChannel,
 		maxHeightByGroupID: make(map[string]uint64),
 		opChannel:          make(chan func()),
 	}
 }
 
 func (c *ChainMetadataStore) Start() {
-	go func() {
-		for update := range c.blockHeightChannel {
-			c.ProcessUpdate(update)
-		}
-	}()
 	go func(c *ChainMetadataStore) {
 		for op := range c.opChannel {
 			op()
 		}
-		//for {
-		//	select {
-		//	//case update := <-c.blockHeightChannel:
-		//	//	c.ProcessUpdate(update)
-		//	case op := <-c.opChannel:
-		//		op()
-		//	}
-		//}
-		//for r := range c.opChan {
-		//	r.fn()
-		//}
-		//for {
-		//	select {
-		//	case update := <-c.blockHeightChannel:
-		//		c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
-		//		c.updateHeightForGroup(update.GroupID, update.BlockHeight)
-		//	case readRequest := <-c.readRequestChannel:
-		//		if readRequest.isGlobal {
-		//			c.readResponseChannel <- c.globalMaxHeight
-		//		} else {
-		//			c.readResponseChannel <- c.maxHeightByGroupID[readRequest.groupID]
-		//		}
-		//	}
-		//	//var update = <-c.blockHeightChannel
-		//	//c.globalMaxHeight = max(c.globalMaxHeight, update.BlockHeight)
-		//	////atomic.
-		//	//c.updateHeightForGroup(update.GroupID, update.BlockHeight)
-		//}
 	}(c)
 }
 
