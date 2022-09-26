da21a8cd67d355fce4ae1102f76921adbb1ed56e
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2022-09-26 17:32:18 +0300

    feat: add NearGlobalMaxHeight filter

--- internal/route/node_filter.go
+++ internal/route/node_filter.go
@@ -6,6 +6,10 @@ import (
 	"github.com/satsuma-data/node-gateway/internal/metadata"
 )
 
+const (
+	MaxAllowedBlocksBehindHead = 10
+)
+
 type NodeFilter interface {
 	Apply(requestMetadata metadata.RequestMetadata, upstreamConfig *config.UpstreamConfig) bool
 }
@@ -37,17 +41,21 @@ func (f *IsHealthy) Apply(_ metadata.RequestMetadata, upstreamConfig *config.Ups
 	return upstreamStatus.PeerCheck.IsPassing() && upstreamStatus.SyncingCheck.IsPassing()
 }
 
-type IsAtGlobalMaxHeight struct {
+type IsCloseToGlobalMaxHeight struct {
 	healthCheckManager checks.HealthCheckManager
 	chainMetadataStore *metadata.ChainMetadataStore
+	maxBlocksBehind    uint64
 }
 
-func (f *IsAtGlobalMaxHeight) Apply(_ metadata.RequestMetadata, upstreamConfig *config.UpstreamConfig) bool {
+func (f *IsCloseToGlobalMaxHeight) Apply(_ metadata.RequestMetadata, upstreamConfig *config.UpstreamConfig) bool {
 	maxHeight := f.chainMetadataStore.GetGlobalMaxHeight()
 
 	upstreamStatus := f.healthCheckManager.GetUpstreamStatus(upstreamConfig.ID)
 
-	return upstreamStatus.BlockHeightCheck.IsPassing(maxHeight)
+	checkIsHealthy := upstreamStatus.BlockHeightCheck.GetError() == nil
+	isClose := upstreamStatus.BlockHeightCheck.GetBlockHeight()+f.maxBlocksBehind >= maxHeight
+
+	return checkIsHealthy && isClose
 }
 
 type IsAtMaxHeightForGroup struct {
@@ -98,9 +106,16 @@ func CreateSingleNodeFilter(
 	case Healthy:
 		return &IsHealthy{manager}
 	case GlobalMaxHeight:
-		return &IsAtGlobalMaxHeight{
+		return &IsCloseToGlobalMaxHeight{
 			healthCheckManager: manager,
 			chainMetadataStore: store,
+			maxBlocksBehind:    0,
+		}
+	case NearGlobalMaxHeight:
+		return &IsCloseToGlobalMaxHeight{
+			healthCheckManager: manager,
+			chainMetadataStore: store,
+			maxBlocksBehind:    MaxAllowedBlocksBehindHead,
 		}
 	case MaxHeightForGroup:
 		return &IsAtMaxHeightForGroup{
@@ -117,8 +132,9 @@ func CreateSingleNodeFilter(
 type NodeFilterType string
 
 const (
-	Healthy            NodeFilterType = "healthy"
-	GlobalMaxHeight    NodeFilterType = "globalMaxHeight"
-	MaxHeightForGroup  NodeFilterType = "maxHeightForGroup"
-	SimpleStatePresent NodeFilterType = "simpleStatePresent"
+	Healthy             NodeFilterType = "healthy"
+	GlobalMaxHeight     NodeFilterType = "globalMaxHeight"
+	NearGlobalMaxHeight NodeFilterType = "nearGlobalMaxHeight"
+	MaxHeightForGroup   NodeFilterType = "maxHeightForGroup"
+	SimpleStatePresent  NodeFilterType = "simpleStatePresent"
 )
