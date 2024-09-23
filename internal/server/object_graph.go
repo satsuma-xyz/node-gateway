581ff9fe1e0f7a3e1d985432bbe8bdd544ae0a7f
Peter Olsar <peter.olsar@alchemy.com>
2024-09-22 17:02:33 -0700

    fix: improve readability

--- internal/server/object_graph.go
+++ internal/server/object_graph.go
@@ -53,7 +53,7 @@ func wireSingleChainDependencies(
 	}
 
 	// TODO(polsar): Here, the HealthCheckManager is wired into the primary FilteringRoutingStrategy.
-	// We may need to wire it into the secondary PriorityRoundRobinStrategy as well.
+	//  We may need to wire it into the secondary PriorityRoundRobinStrategy as well.
 	enabledNodeFilters := []route.NodeFilterType{route.Healthy, route.MaxHeightForGroup, route.MethodsAllowed, route.NearGlobalMaxHeight}
 	nodeFilter := route.CreateNodeFilter(enabledNodeFilters, healthCheckManager, chainMetadataStore, logger, &chainConfig.Routing)
 	routingStrategy := route.FilteringRoutingStrategy{
