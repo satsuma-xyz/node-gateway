1d92136c13c2d85fc080ff8876530a71b17c9f61
Peter Olsar <peter.olsar@alchemy.com>
2024-10-03 19:29:28 -0700

    feat: initial implementation of `AlwaysRouteRoutingStrategy`

---
+++ internal/route/always_route_filtering_strategy.go
@@ -0,0 +1,132 @@
+package route
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/satsuma-data/node-gateway/internal/config"
+	"github.com/satsuma-data/node-gateway/internal/metadata"
+	"github.com/satsuma-data/node-gateway/internal/types"
+	"go.uber.org/zap"
+)
+
+type AlwaysRouteRoutingStrategy struct {
+	BackingStrategy  RoutingStrategy
+	Logger           *zap.Logger
+	NodeFilters      []NodeFilter
+	RemovableFilters []NodeFilterType
+}
+
+func (s *AlwaysRouteRoutingStrategy) RouteNextRequest(
+	upstreamsByPriority types.PriorityToUpstreamsMap,
+	requestMetadata metadata.RequestMetadata,
+) (string, error) {
+	// Create a copy of removable filters to avoid modifying the original slice.
+	removableFilters := make([]NodeFilterType, len(s.RemovableFilters))
+	copy(removableFilters, s.RemovableFilters)
+
+	filters := s.NodeFilters // WARNING: Slice assignment, not a copy!
+	for len(filters) > 0 {
+		// Get all healthy upstreams for the current set of filters.
+		upstreams := s.FilterUpstreams(upstreamsByPriority, requestMetadata, filters)
+
+		// If there is at least one healthy upstream, route using the backing strategy.
+		if len(upstreams) > 0 {
+			return s.BackingStrategy.RouteNextRequest(upstreams, requestMetadata)
+		}
+
+		// If there are no more filters to remove, and there are no healthy upstreams
+		// found, so give up.
+		if len(removableFilters) == 0 {
+			break
+		}
+
+		// Remove the next filter and try again.
+		idx := len(removableFilters) - 1
+		nextFilterToRemove := removableFilters[idx]
+		removableFilters = removableFilters[:idx]
+		filters = removeFilters(filters, nextFilterToRemove)
+	}
+
+	// If all removable filters are exhausted and no healthy upstreams are found,
+	// pass in the original list of upstreams to the backing strategy.
+	return s.BackingStrategy.RouteNextRequest(upstreamsByPriority, requestMetadata)
+}
+
+// FilterUpstreams filters upstreams based on the provided NodeFilter.
+// WARNING: If a given priority does not have any healthy upstreams,
+//
+//	it will not be included in the returned map.
+func (s *AlwaysRouteRoutingStrategy) FilterUpstreams(
+	upstreamsByPriority types.PriorityToUpstreamsMap,
+	requestMetadata metadata.RequestMetadata,
+	nodeFilters []NodeFilter,
+) types.PriorityToUpstreamsMap {
+	priorityToHealthyUpstreams := make(types.PriorityToUpstreamsMap)
+
+	// Iterate over each priority and filter the associated upstreams.
+	for priority, upstreamConfigs := range upstreamsByPriority {
+		s.Logger.Debug(
+			"Determining healthy upstreams at priority.",
+			zap.Int("priority", priority),
+			zap.Any("upstreams", upstreamConfigs),
+		)
+
+		filteredUpstreams := make([]*config.UpstreamConfig, 0)
+
+		// Iterate over each upstream and apply the filters.
+		for _, upstreamConfig := range upstreamConfigs {
+			pass := true
+			for _, nodeFilter := range nodeFilters {
+				pass = nodeFilter.Apply(requestMetadata, upstreamConfig, len(upstreamConfigs))
+				if !pass {
+					break
+				}
+			}
+
+			if pass {
+				filteredUpstreams = append(filteredUpstreams, upstreamConfig)
+			}
+		}
+
+		// Only add the priority to the map if there is at least one healthy upstream.
+		if len(filteredUpstreams) > 0 {
+			priorityToHealthyUpstreams[priority] = filteredUpstreams
+		}
+	}
+
+	return priorityToHealthyUpstreams
+}
+
+// removeFilters returns a new slice of filters with the given filter type removed.
+func removeFilters(filters []NodeFilter, filterToRemove NodeFilterType) []NodeFilter {
+	retFilters := make([]NodeFilter, 0)
+
+	for _, filter := range filters {
+		if getFilterTypeName(filter) != filterToRemove {
+			retFilters = append(retFilters, filter)
+		}
+	}
+
+	return retFilters
+}
+
+func getFilterTypeName(v interface{}) NodeFilterType {
+	t := reflect.TypeOf(v)
+
+	// If it's a pointer, get the element type.
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	// Extract the name of the type and remove the package path.
+	typeName := t.String()
+	lastDotIndex := strings.LastIndex(typeName, ".")
+
+	if lastDotIndex != -1 {
+		// Remove the package path, keep only the type name.
+		typeName = typeName[lastDotIndex+1:]
+	}
+
+	return NodeFilterType(typeName)
+}
---
+++ internal/route/always_route_filtering_strategy_test.go
@@ -0,0 +1,83 @@
+package route
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getFilterTypeName(t *testing.T) {
+	Assert := assert.New(t)
+
+	Assert.Equal(
+		NodeFilterType("AlwaysPass"),
+		getFilterTypeName(AlwaysPass{}),
+	)
+	Assert.Equal(
+		NodeFilterType("AlwaysFail"),
+		getFilterTypeName(AlwaysFail{}),
+	)
+	Assert.Equal(
+		NodeFilterType("AndFilter"),
+		getFilterTypeName(&AndFilter{}),
+	)
+}
+
+func Test_RemoveFilters_RemoveNone(t *testing.T) {
+	Assert := assert.New(t)
+
+	Assert.Equal(
+		[]NodeFilter{},
+		removeFilters([]NodeFilter{}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysFail{}},
+		removeFilters([]NodeFilter{AlwaysFail{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysFail{}, &AndFilter{}},
+		removeFilters([]NodeFilter{AlwaysFail{}, &AndFilter{}}, "AlwaysPass"),
+	)
+}
+
+func Test_RemoveFilters_RemoveOne(t *testing.T) {
+	Assert := assert.New(t)
+
+	Assert.Equal(
+		[]NodeFilter{},
+		removeFilters([]NodeFilter{AlwaysPass{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysFail{}},
+		removeFilters([]NodeFilter{AlwaysFail{}, AlwaysPass{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{&AndFilter{}},
+		removeFilters([]NodeFilter{AlwaysPass{}, &AndFilter{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysPass{}},
+		removeFilters([]NodeFilter{AlwaysPass{}, &AndFilter{}}, "AndFilter"),
+	)
+}
+
+func Test_RemoveFilters_RemoveTwo(t *testing.T) {
+	Assert := assert.New(t)
+
+	Assert.Equal(
+		[]NodeFilter{},
+		removeFilters([]NodeFilter{AlwaysPass{}, AlwaysPass{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{&AndFilter{}},
+		removeFilters([]NodeFilter{AlwaysPass{}, &AndFilter{}, AlwaysPass{}}, "AlwaysPass"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysPass{}},
+		removeFilters([]NodeFilter{AlwaysPass{}, &AndFilter{}, &AndFilter{}}, "AndFilter"),
+	)
+	Assert.Equal(
+		[]NodeFilter{AlwaysFail{}},
+		removeFilters([]NodeFilter{AlwaysPass{}, AlwaysPass{}, AlwaysFail{}}, "AlwaysPass"),
+	)
+}
